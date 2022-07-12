51f5c96d8095e1ef4a763db0336b275cb486ed56
leonlee021 <leonlee021@gmail.com>
2022-07-12 14:41:06 +0900

    edit form for personal info complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react' 
-import Personal from './Personal'
 
 export default function Header(){
+    const [personalFormData, setPersonalFormData] = React.useState(
+        {
+            fullName: "",
+            address: "",
+            phone: "",
+            email: "",
+        }
+    )
+
+    function handleChange(event){
+        console.log(event.target.value)
+        const {name, value} = event.target
+        setPersonalFormData(prevPersonalFormData => {
+            return{
+                ...prevPersonalFormData,
+                [name] : value
+            }
+        })
+    }
+
     const [edit, setEdit] = React.useState(false)
 
     function toggleEdit(){
@@ -11,14 +30,29 @@ export default function Header(){
     return (
         <>
             <div id="cv-header" className = "cv-header">
-                <div>Name</div>
-                <div id="cv-personal">
-                    <div>Address</div>
-                    <div>Phone Number</div>
-                    <div>Email</div>
+                <div id="name-button-row">
+                    <div>{personalFormData.fullName ? personalFormData.fullName : 'Full Name'}</div>
+                    <button type="button" className="edit-name" onClick={toggleEdit}>{edit ? 'CLOSE EDIT' : 'EDIT'}</button>
                 </div>
-            <button type="button" className="edit-name" onClick={toggleEdit}>EDIT</button>
-            {edit && <Personal handleClick = {toggleEdit} />}
+                <div id="cv-personal">
+                    <div>{personalFormData.address ? personalFormData.address : 'Address'}</div>
+                    <div>{personalFormData.phone ? personalFormData.phone : 'Phone Number'}</div>
+                    <div>{personalFormData.email ? personalFormData.email : 'Email'}</div>
+                </div>
+            {edit && 
+                <div className="form-popup">
+                    <form id="personal-form">
+                        <div className="personal-row">
+                            <input className = "personal" type="text" id = "fullName" name = "fullName" placeholder="Name" value={personalFormData.fullName} onChange={handleChange}/>
+                            <input className = "personal" type="text" id = "address" name = "address" placeholder="Address" value={personalFormData.address} onChange={handleChange}/>
+                        </div>
+                        <div className="personal-row">
+                            <input className = "personal" type="text" id = "phone" name = "phone" placeholder="Phone Number" value={personalFormData.phone} onChange={handleChange}/>
+                            <input className = "personal" type="text" id = "email" name = "email" placeholder="Email" value={personalFormData.email} onChange={handleChange}/>
+                        </div>
+                    </form>
+                </div>
+            }
             </div>
         </>
     )
